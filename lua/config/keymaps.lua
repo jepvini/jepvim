e6372e39dfd42140dbe09c1d16c1f17bc2d8a18b
jep vini <leos1359531@gmail.com>
2025-11-15 12:37:04 +0100

    changed buffer change keys

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -51,8 +51,8 @@ vim.keymap.set("v", "<Up>", "<nop>")
 vim.keymap.set("v", "<Down>", "<nop>")
 
 -- Move in buffers
-vim.keymap.set("n", "<C-k>", "<cmd>bnext<cr>")
-vim.keymap.set("n", "<C-j>", "<cmd>bprevious<cr>")
+vim.keymap.set({ "n", "i" }, "<C-l>", "<cmd>bnext<cr>")
+vim.keymap.set({ "n", "i" }, "<C-h>", "<cmd>bprevious<cr>")
 
 vim.api.nvim_set_keymap(
   "n",
